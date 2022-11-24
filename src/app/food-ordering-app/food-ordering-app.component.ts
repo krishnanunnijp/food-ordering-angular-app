285c4f8e14a746151b9537b581e511ef4ee5c582
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-24 19:48:12 +0530

    login page added

---
+++ src/app/food-ordering-app/food-ordering-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-ordering-app',
+  templateUrl: './food-ordering-app.component.html',
+  styleUrls: ['./food-ordering-app.component.css']
+})
+export class FoodOrderingAppComponent {
+
+}

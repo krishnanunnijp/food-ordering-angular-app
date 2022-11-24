f35792d208610d07812d0564935af4ab4d5f51e8
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-24 20:30:53 +0530

    arabian food  page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { FoodOrderingAppComponent } from './food-ordering-app/food-ordering-app.component';
 import { IndianFoodComponent } from './indian-food/indian-food.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { ArabianFoodComponent } from './arabian-food/arabian-food.component';
 
 const myRoute : Routes =[
   {
@@ -20,6 +21,10 @@ const myRoute : Routes =[
   {
     path: "signup",
     component: SignUpPageComponent
+  },
+  {
+    path: "arabian",
+    component: ArabianFoodComponent
   }
 ]
 
@@ -28,7 +33,8 @@ const myRoute : Routes =[
     AppComponent,
     FoodOrderingAppComponent,
     IndianFoodComponent,
-    SignUpPageComponent
+    SignUpPageComponent,
+    ArabianFoodComponent
   ],
   imports: [
     BrowserModule,
